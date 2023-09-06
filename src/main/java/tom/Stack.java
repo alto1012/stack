be15b69d509c0dc658043ee08bc02875769005c2
Tom <tom.albrecht.privat@gmail.com>
2023-09-06 20:35:58 +0200

    test

--- src/main/java/tom/Stack.java
+++ src/main/java/tom/Stack.java
@@ -1,29 +1,24 @@
 package tom;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Stack implements StackInterface {
-    private List<Object> elements = new ArrayList<>();
+    private Object[] elements;
     private int size = 0;
 
 
     public Object push(Object item) {
-        elements.add(item);
-        size++;
+        elements[size++] = item;
         return item;
     }
 
     public Object pop() {
         Object item = peek();
-        size--;
-        elements.remove(size);
+        elements[--size] = null;
         return item;
     }
 
     public Object peek() {
-        return elements.get(size - 1);
+        return elements[size - 1];
     }
 
     public int size() {
